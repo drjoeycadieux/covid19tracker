7be314c31146ff765563ce8adb6b2aade1e5c6a5
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-04-27 16:31:48 -0500

    dev header

--- vue-covid-tracker/src/components/Header.vue
+++ vue-covid-tracker/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="text-center bg-blue-800 text-white p-4 md:pt-20 md:pb-20">
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
     <div class="text-3xl md;text-5xl font-bold mb-3">
       <i class="fa fa-viruses"></i> Covid-19 Tracker
     </div>
